84027adf40a027955580edbc37b3cbc743285db5
mathtml <matheusfgoncalves12@gmail.com>
2024-12-20 08:12:17 -0300

    criação do projeto

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: '/:path*',
-        destination: '/pages/:path*', // Remap to /app/pages path
+        destination: '/pages/:path*', 
       },
     ];
   },
